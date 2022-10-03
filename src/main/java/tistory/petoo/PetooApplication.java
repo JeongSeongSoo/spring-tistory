dc33445298ed38952ec5f03cc47b1201dd9a6359
ssjeong <ssjeong@wbsoft.kr>
2022-10-03 17:00:41 +0900

    [프뚜] > [JAVA] 파일 이름, 경로, 확장자명 나누기

--- src/main/java/tistory/petoo/PetooApplication.java
+++ src/main/java/tistory/petoo/PetooApplication.java
@@ -13,9 +13,23 @@ public class PetooApplication {
 	}
 
 	private static void FILE() throws Exception {
-		String fullFilePath = "D:\\Doc\\ssjeong\\프뚜.png";
-		FilenameUtils.getExtension("");
-		System.out.println("[프뚜] > ");
+		String absolutePath = "D:\\ssjeong\\프뚜.png";
+		String name = FilenameUtils.getName(absolutePath);
+		String fullPath = FilenameUtils.getFullPath(absolutePath);
+		String baseName = FilenameUtils.getBaseName(absolutePath);
+		String extension = FilenameUtils.getExtension(absolutePath);
+
+		// 2022.10.03[프뚜]: 확장자를 제거한 순수 파일명
+		System.out.println("[프뚜] > 01. 파일명: " + baseName);
+
+		// 2022.10.03[프뚜]: 파일명을 제거한 순수 확장자
+		System.out.println("[프뚜] > 02. 확장자: " + extension);
+
+		// 2022.10.03[프뚜]: 파일이 있는 경로
+		System.out.println("[프뚜] > 03. 파일경로: " + fullPath);
+
+		// 2022.10.03[프뚜]: 파일의 이름과 확장자
+		System.out.println("[프뚜] > 04. 파일명 + 확장자: " + name);
 	}
 
 }
