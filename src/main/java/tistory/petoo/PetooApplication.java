bd7ef5c007abfac4524881e65ed30a57fe0d5c0a
ssjeong <ssjeong@wbsoft.kr>
2022-10-03 16:36:14 +0900

    [프뚜] 임시커밋

--- src/main/java/tistory/petoo/PetooApplication.java
+++ src/main/java/tistory/petoo/PetooApplication.java
@@ -1,55 +1,21 @@
 package tistory.petoo;
 
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.OperationContext;
-import com.microsoft.azure.storage.blob.BlobRequestOptions;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @SpringBootApplication
 public class PetooApplication {
 
 	public static void main(String[] args) throws Exception {
-		long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
-
-		AZURE_BLOB_UPLOAD();
-
-		long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
-		long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
-		System.out.println("시간차이(m) : "+secDiffTime);
-
 		// SpringApplication.run(PetooApplication.class, args);
+		FILE();
 	}
 
-	private static void AZURE_BLOB_UPLOAD() throws Exception {
-		// 2022.09.27[프뚜]: 스토어 연결을 위해 인증
-		String storageConnectionString =
-				"DefaultEndpointsProtocol=https;" +
-						"AccountName=<account-name/>;" +
-						"AccountKey=<account-key/>";
-
-		// 2022.09.27[프뚜]: 스토리지 계정 > 데이터 스토리지 > 컨테이너 연결
-		CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
-		CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
-
-		// 2022.09.27[프뚜]: 컨테이너명
-		CloudBlobContainer container = blobClient.getContainerReference("ssjeong");
-		container.createIfNotExists(new BlobRequestOptions(), new OperationContext());
-
-		// 2022.09.27[프뚜]: 업로드 대상
-		Path path = Paths.get("D:\\Doc\\ssjeong\\test.mp4");
-
-		// 2022.09.27[프뚜]: 컨테이너에 저장 > 경로 + 파일명, 타입 지정
-		CloudBlockBlob blob = container.getBlockBlobReference("example/" + path.toFile().getName());
-		blob.getProperties().setContentType("video/mp4");
-
-		// 2022.09.27[프뚜]: blob 업로드 실행
-		blob.uploadFromFile(path.toFile().getAbsolutePath());
+	private static void FILE() throws Exception {
+		String fullFilePath = "D:\\Doc\\ssjeong\\프뚜.png";
+		FilenameUtils.getExtension("");
+		System.out.println("[프뚜] > ");
 	}
 
 }
--- src/main/java/tistory/petoo/TestController.java
+++
@@ -1,8 +0,0 @@
-package tistory.petoo;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class TestController {
-
-}
